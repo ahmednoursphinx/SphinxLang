a94fba9ad728c1b3f8deaae032f0aa6501a2a9d0
ahmed <ahmednour.mohamed2012@gmail.com>
2025-11-13 16:38:30 +0200

    feat: Add TokenKind enum

--- Lexer.cpp
+++ Lexer.cpp
@@ -4,3 +4,22 @@
 #include "llvm/ADT/StringRef.h" // llvm::StringRef class encapsulates a pointer to a C string and its length
 #include "llvm/Support/MemoryBuffer.h" // llvm::MemoryBuffer class provides read-only access to a block of memory, filled with the content of a file
 
+class Token {
+
+public:
+  enum TokenKind : unsigned short {
+    eoi, // end of input and is returned when all characters of the input are processed
+    unknown, // used in the event of an error at the lexical level
+    ident,
+    number,
+    comma,
+    colon,
+    plus,
+    minus,
+    star,
+    slash,
+    l_paren,
+    r_paren,
+    KW_with
+  };
+}
